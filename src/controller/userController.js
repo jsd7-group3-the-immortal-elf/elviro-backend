b15eb880eb3d35189e3234367c65754d109bb5d5
Phatthawut <42006909+Phatthawut@users.noreply.github.com>
2024-07-08 12:08:23 +0700

    add get user by id

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,6 +1,6 @@
 import userModel from "../model/userModel.js";
 
-const getAllUsers = async (req, res) => {
+export const getAllUsers = async (req, res) => {
 	try {
 		const allUsers = await userModel.find();
 		res.status(200).json({
@@ -14,4 +14,18 @@ const getAllUsers = async (req, res) => {
 	}
 };
 
-export default getAllUsers;
+export const getUserById = async (req, res) => {
+	try {
+		const { userId } = req.params;
+		const user = await userModel.findById(userId);
+
+		res.status(200).json({
+			message: `get user id ${userId} success`,
+			data: user,
+		});
+	} catch (error) {
+		res.status(404).json({
+			message: "get user failed",
+		});
+	}
+};
--- src/route/userRoute.js
+++ src/route/userRoute.js
@@ -1,4 +1,9 @@
 import express from "express";
+import {
+	getAllUsers,
+	getUserById,
+} from "../controller/userController.js";
+
 // import {
 // 	getAllUsers,
 // 	getUserById,
@@ -7,16 +12,13 @@ import express from "express";
 // 	deleteUser,
 // } from "../controller/userController.js";
 
-import getAllUsers from "../controller/userController.js";
-
-
 // import adminAuthMiddleware from "../middleware/adminAuthMiddleware";
 
 const router = express.Router();
 
 router.get("/", getAllUsers);
 
-// router.get("/:userId", getUserById);
+router.get("/:userId", getUserById);
 
 // router.get("/browse", async (req, res, next) => {});
 
